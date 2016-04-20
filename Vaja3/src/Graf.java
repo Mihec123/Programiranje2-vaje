e088b3eb369bbe7cbf19a88d09020b1874614bfc
Mihec123 <mihavsec@gmail.com>
2016-04-20 14:00:06 +0200

    vaja 3

--- Vaja3/src/Graf.java
+++ Vaja3/src/Graf.java
@@ -1,20 +1,21 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 
 public class Graf {
-	
+
 	public Map<Object,Tocka> tocke;
 
 	public Graf() {
 		this.tocke = new HashMap<Object,Tocka>();
 	}
-	
+
 	public Tocka tocka(Object ime){
 		return tocke.get(ime);
-		
+
 	}
-	
+
 	public Boolean povezava(Object prva, Object druga){
 		Boolean jePovezava = false;
 		if(tocke.get(prva).sosedi.contains(druga)){
@@ -22,64 +23,64 @@ public class Graf {
 		}
 		return jePovezava;
 	}
-	
+
 	public void dodajTocko(Object a){
 		if(!tocke.containsKey(a)){
 			this.tocke.put(a, new Tocka(a));
 		}
-		
+
 	}
-	
+
 	public void dodajPovezavo(Tocka a,Tocka b){
 		if(a != b){
 			a.sosedi.add(b);
 			b.sosedi.add(a);
 		}
 	}
-	
+
 	public void odstraniPovezavo(Tocka a,Tocka b){
-			a.sosedi.remove(b);
-			b.sosedi.remove(a);
+		a.sosedi.remove(b);
+		b.sosedi.remove(a);
 	}
-	
+
 	public void odstraniTocko(Tocka a){
 		for(Tocka el: a.sosedi){
 			odstraniPovezavo(el,a);
 		}
 		this.tocke.remove(a);
 	}
-	
+
 	public static Graf prazen(int n){
 		Graf ntock = new Graf();
 		for(int i = 0; i < n; i++){
 			ntock.dodajTocko(new Tocka(i));
 		}
 		return ntock;
-		
+
 	}
-	
+
 	public static Graf cikel(int n){
 		Graf ciku = Graf.prazen(n);
 		for(int i = 1; i < n; i++){
 			ciku.dodajPovezavo(ciku.tocka(i), ciku.tocka(i-1));
 		}
 		ciku.dodajPovezavo(ciku.tocka(0), ciku.tocka(n-1));
-		
+
 		return ciku;
-		
+
 	}
-	
+
 	public static Graf poln(int n){
 		Graf poln = Graf.prazen(n);
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < i; j++){
-					poln.dodajPovezavo(poln.tocka(i), poln.tocka(j));	
+				poln.dodajPovezavo(poln.tocka(i), poln.tocka(j));	
 			}
-			
+
 		}
 		return poln;
 	}
-	
+
 	public static Graf polnDvodelen(int n, int m){
 		Graf dvodelen = Graf.prazen(n+m);
 		for(int i = 0; i < n; i++){
@@ -89,9 +90,22 @@ public class Graf {
 		}
 		return dvodelen;
 	}
-	
-	
-	
-	
+
+	public Boolean povezan(Graf a){
+		Tocka b = a.tocke.values().iterator().next();
+		Vector<Tocka> povezave = new Vector<Tocka>();
+		povezave.add(b);
+		for (Tocka j: povezave){
+		for(Tocka i : j.sosedi){
+			if (! povezave.contains(i)){
+				povezave.add(i);
+			}
+		}
+		}
+		return povezave.size() == a.tocke.size();
+
+	}
+
+
 
 }
